5bdd4db12b735f30b9522c6db68e3d39dade998b
Yun Ni <ni.yun@outlook.com>
2021-12-01 23:37:01 -0500

    add syn and ack, but its not working

--- httpc_lib.py
+++ httpc_lib.py
@@ -1,15 +1,101 @@
+import ipaddress
 import os
 import json
 import socket
+import argparse
+from urllib import request
+
 from env import Debug
 from urllib.parse import urlparse
-
+from packet import Packet
 
 # It supports 330 Redirections due to Recursion Stack
 # import sys
 # print(sys.getrecursionlimit())
 # sys.setrecursionlimit(1500)
 # print(sys.getrecursionlimit())
+
+def syn(router_addr, router_port, server_addr, server_port):
+    while True:
+        peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
+        conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        timeout = 5
+        try:
+            p = Packet(packet_type=1,
+                       seq_num=1,
+                       peer_ip_addr=peer_ip,
+                       peer_port=server_port,
+                       payload=request.encode("utf-8"))
+
+            conn.sendto(p.to_bytes(), (router_addr, router_port))
+            print(" \n ")
+            print("-------------------BEGINNING HANDSHAKE-----------------")
+            print("[CLIENT] - Sending SYN - (PacketType = 1)")
+            conn.settimeout(timeout)
+            print('[CLIENT] - Waiting For A Response - Should be an SYN-ACK')
+            response, sender = conn.recvfrom(1024)
+            p = Packet.from_bytes(response)
+            print("[CLIENT] - Response Recieved. Is it a SYN-ACK? (Packet Type of 2)")
+            print('[CLIENT] - PacketType =  ', p.packet_type)
+
+            if p.packet_type == 2:
+                print("[CLIENT] - Yes, Got a SYN-ACK back, send back ACK (Packet Type of 3)")
+                # just fucking send packet of type 3 send here and don't get anything back.
+                return True
+
+        except socket.timeout:
+            print('[CLIENT] - No response after %d for Packet %d ' % (timeout, p.seq_num))
+        finally:
+            conn.close()
+
+
+def ack(router_addr, router_port, server_addr, server_port):
+    while True:
+        peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
+        conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        timeout = 5
+        try:
+            # Packet type to 1 (SYN). Then have the server recognize the packet_type and return a 2 (SYN-ACK)
+            p = Packet(packet_type=3,
+                       seq_num=1,
+                       peer_ip_addr=peer_ip,
+                       peer_port=server_port,
+                       payload=request.encode("utf-8"))
+            print("[CLIENT] - Sending ACK")
+            conn.sendto(p.to_bytes(), (router_addr, router_port))
+
+            # Receive a response within timeout
+            conn.settimeout(timeout)
+            print("[CLIENT] - Waiting For A Response -  (Should be an ACK)")
+            response, sender = conn.recvfrom(1024)
+            p = Packet.from_bytes(response)
+
+            print("[CLIENT] - Response Recieved. Is it a SYN-ACK? (Packet of Type 3)")
+            print('[CLIENT] - PacketType = ', p.packet_type)
+            print("[CLIENT] - Yes, Got an ACK back. Proceed with request.")
+            return True
+
+        except socket.timeout:
+            print('[CLIENT] - No response after %ds for Packet %d ' % (timeout, p.seq_num))
+        finally:
+            conn.close()
+
+
+def handshake(routerhost="localhost", routerport="3000", serverhost="localhost", serverport="8007"):
+    handShake = False
+    # Always perform a handshake before initial request.
+    while not handShake:
+        sendSyn = False
+        sendSyn = syn(routerhost, routerport, serverhost, serverport)
+
+        # Only return true when the whole thing comes back. check at each step.
+        if sendSyn:
+            sendAck = ack(routerhost, routerport, serverhost, serverport)
+            if sendAck:
+                print("--------------------HANDSHAKE COMPLETE-----------------")
+                handShake = True
+    return True
+
 
 class httpclient:
     """This is the http class for client side operations"""
@@ -80,7 +166,7 @@ class httpclient:
         if self.is_post:
             self.post()
 
-    def get(self):
+    def get(self, routerhost="localhost", routerport="3000", serverhost="localhost", serverport="8007"):
         """ Build GET and send to host"""
         request = ""
         request += "GET " + self.path + "?" + self.query + " HTTP/1.1\r\n"
@@ -100,10 +186,13 @@ class httpclient:
         if Debug: print("""-------------------------RESPONSE------------------------""")
 
         # Pass this data to TCP Client
-        self.is_get = True
-        self.run_client(request)
+        # self.is_get = True
+        # self.run_client(request)
+        handShakeComplete = handshake()
+        if handShakeComplete:
 
-    def post(self):
+
+    def post(self, routerhost="localhost", routerport="3000", serverhost="localhost", serverport="8007"):
         """ Build POST and send to host"""
 
         # Guard
@@ -155,38 +244,69 @@ class httpclient:
         if Debug: print(request)
         if Debug: print("""-------------------------RESPONSE------------------------""")
 
-        self.is_post = True
-        # Pass this data to TCP Client
-        self.run_client(request)
+        # self.is_post = True
+        # # Pass this data to TCP Client
+        # self.run_client(request)
+        handShakeComplete = handshake()
 
-    def run_client(self, request):
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #  This allows the address/port to be reused immediately instead of
-        #  it being stuck in the TIME_WAIT state for several minutes, waiting for late packets to arrive.
-        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        try:
-            client.connect((self.host, self.port))
-            request = request.encode(self.FORMAT)
-            client.sendall(request)
-            # MSG_WAITALL waits for full request or error
-            response = client.recv(self.BUFFER_SIZE)
 
-            response = response.decode(self.FORMAT)
-            response = response.split("\r\n\r\n")
+    def run_client(self,router_addr, router_port, server_addr, server_port, sequence_number, request):
+        # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # #  This allows the address/port to be reused immediately instead of
+        # #  it being stuck in the TIME_WAIT state for several minutes, waiting for late packets to arrive.
+        # client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # try:
+        #     client.connect((self.host, self.port))
+        #     request = request.encode(self.FORMAT)
+        #     client.sendall(request)
+        #     # MSG_WAITALL waits for full request or error
+        #     response = client.recv(self.BUFFER_SIZE)
+        #
+        #     response = response.decode(self.FORMAT)
+        #     response = response.split("\r\n\r\n")
+        #
+        #     # Check Response
+        #     if len(response) >= 2:
+        #         if self.is_redirect(response) and self.redirection:
+        #             self.redirect(response)
+        #         else:
+        #             if self.verbose:
+        #                 print(response[0].strip(), "\n")
+        #             print(response[1].strip())
+        #             if self.output_file is not None:
+        #                 self.save_as_file(response[1].strip())
+        #     else:
+        #         if Debug:
+        #             print("Response:\n", response, "\n")
+        #         print("Oops! Something went wrong ;(")
+        # finally:
+        #     client.close()
+        while True:
+            peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
+            conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            timeout = 5
+            try:
+                p = Packet(packet_type=0,
+                           seq_num=sequence_number,
+                           peer_ip_addr=peer_ip,
+                           peer_port=server_port,
+                           payload=request.encode("utf-8"))
 
-            # Check Response
-            if len(response) >= 2:
-                if self.is_redirect(response) and self.redirection:
-                    self.redirect(response)
-                else:
-                    if self.verbose:
-                        print(response[0].strip(), "\n")
-                    print(response[1].strip())
-                    if self.output_file is not None:
-                        self.save_as_file(response[1].strip())
-            else:
-                if Debug:
-                    print("Response:\n", response, "\n")
-                print("Oops! Something went wrong ;(")
-        finally:
-            client.close()
+                conn.sendto(p.to_bytes(), (router_addr, router_port))
+                # print('Send "{}" to rout234er'.format(message))
+                print("[CLIENT] - Sending packet to Router. Sequence Number = ", p.seq_num)
+                # Try to receive a response within timeout
+                conn.settimeout(timeout)
+                # print('Waiting for a response')
+                response, sender = conn.recvfrom(1024)
+                p = Packet.from_bytes(response)
+                # print('Router: ', sender)
+                # print('Packet: ', p)
+                print('[CLIENT] - PayLoad from Packet %d : %s ' % (p.seq_num, p.payload.decode("utf-8")[1:]))
+                # print('[CLIENT] - Payload from Packet: ', p.seq_num, ' - ', p.payload.decode("utf-8"))
+                return True
+
+            except socket.timeout:
+                print('[CLIENT] - No response after %d for Packet %d ' % (timeout, p.seq_num))
+            finally:
+                conn.close()
